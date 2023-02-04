13a6e49803030eb35be85a8f932d6b5b6eafee25
Ben Robinson <37141635+benrobinson12@users.noreply.github.com>
2023-02-04 04:57:27 +0000

    Update washing-machine-VHDL-code-2023.vhd

--- washing-machine-VHDL-code-2023.vhd
+++ washing-machine-VHDL-code-2023.vhd
@@ -1,90 +1,71 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity clock_divider is
-    port ( clk_in : in std_logic;
-           clk_out : out std_logic );
-end clock_divider;
-
-architecture divider_arch of clock_divider is
-    signal count : integer range 0 to 49999 := 0;
-begin
-    process (clk_in)
-    begin
-        if rising_edge(clk_in) then
-            if count = 49999 then
-                count <= 0;
-                clk_out <= not clk_out;
-            else
-                count <= count + 1;
-            end if;
-        end if;
-    end process;
-end divider_arch;
-
-entity washing_machine_fsm is
+entity washing_machine is
     port ( clk : in std_logic;
            reset : in std_logic;
            start : in std_logic;
-           done : out std_logic );
-end washing_machine_fsm;
+           cycle_complete : in std_logic;
+           display : out std_logic_vector(6 downto 0) );
+end washing_machine;
 
-architecture fsm_arch of washing_machine_fsm is
-    type state_type is (idle, wash, rinse, spin, dry);
-    signal state, next_state : state_type;
+architecture machine_arch of washing_machine is
+    type states is (idle, wash, rinse, spin, complete);
+    signal current_state, next_state : states;
+    signal count : integer range 0 to 999 := 0;
 begin
     process(clk, reset)
     begin
         if (reset = '1') then
-            state <= idle;
+            current_state <= idle;
         elsif (clk'event and clk = '1') then
-            state <= next_state;
+            current_state <= next_state;
         end if;
     end process;
     
-    process(state, start)
+    process(current_state, start, cycle_complete, count)
     begin
-        case state is
+        case current_state is
             when idle =>
-                done <= '0';
                 if (start = '1') then
                     next_state <= wash;
                 else
                     next_state <= idle;
                 end if;
             when wash =>
-                done <= '0';
-                next_state <= rinse;
+                if (cycle_complete = '1') then
+                    next_state <= rinse;
+                else
+                    next_state <= wash;
+                end if;
             when rinse =>
-                done <= '0';
-                next_state <= spin;
+                if (cycle_complete = '1') then
+                    next_state <= spin;
+                else
+                    next_state <= rinse;
+                end if;
             when spin =>
-                done <= '0';
-                next_state <= dry;
-            when dry =>
-                done <= '1';
+                if (cycle_complete = '1') then
+                    next_state <= complete;
+                else
+                    next_state <= spin;
+                end if;
+            when complete =>
                 next_state <= idle;
+                count <= count + 1;
             when others =>
-                done <= '0';
                 next_state <= idle;
         end case;
     end process;
-end fsm_arch;
-
-entity seven_segment_display is
-    port ( clk : in std_logic;
-           reset : in std_logic;
-           start : in std_logic;
-           display : out std_logic_vector(6 downto 0) );
-end seven_segment_display;
-
-architecture display_arch of seven_segment_display is
-    signal count : integer range 0 to 999 := 0;
-begin
-    process(clk, reset, start)
-    begin
-        if (reset = '1') then
-            count <= 0;
-        elsif (clk'event and clk = '1' and start = '1') then
-            if count = 999 then
-                count <= 0;
+    
+    display <= "1000000" when count = 0 else
+              "1111001" when count = 1 else
+              "0100100" when count = 2 else
+              "0110000" when count = 3 else
+              "0011001" when count = 4 else
+              "0010010" when count = 5 else
+              "0000010" when count = 6 else
+              "1111000" when count = 7 else
+              "0000000" when count = 8 else
+              "0010000";
+end machine_arch;
